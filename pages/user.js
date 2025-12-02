84c14add85229c56d9b1d784c7706c6bd48421b5
JDSEC_DeVSecOps <jacksingh4180@gmail.com>
2025-12-02 22:30:35 +0530

    Update user.js

--- pages/user.js
+++ pages/user.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./database/demo.db");
 
-// ❌ Fully IDOR — no auth check, no owner validation
+//  Fully IDOR — no auth check, no owner validation
 export default function handler(req, res) {
   const id = req.query.id || "1";
 
@@ -10,6 +10,6 @@ export default function handler(req, res) {
 
   db.get(q, (err, row) => {
     if (!row) return res.json({ error: "User not found" });
-    res.json(row); // ❌ return full user details without verification
+    res.json(row); //  return full user details without verification
   });
 }
